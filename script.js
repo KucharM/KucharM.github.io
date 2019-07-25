7fe8d0daf56a418ab6a49062bb8c5cf787707ddf
mm.kucharski@gmail.com <mm.kucharski@gmail.com>
2019-07-25 18:29:10 +0200

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://tranquil-ocean-70433.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://tranquil-ocean-70433.herokuapp.com/v1/trello/';
+  const apiRoot = 'http://localhost:8080/v1/task/';
+  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
@@ -8,7 +8,6 @@ $(document).ready(function() {
   var availableTasks = {};
 
   // init
-
 
   getAllTasks();
 
