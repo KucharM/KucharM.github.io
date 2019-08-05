91091698db062fd615c3b2b4ccd5741594df06ff
mm.kucharski@gmail.com <mm.kucharski@gmail.com>
2019-08-05 20:44:37 +0200

    final refactor

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'https://tranquil-ocean-70433.herokuapp.com/v1';
-  const trelloApiRoot = 'https://tranquil-ocean-70433.herokuapp.com/v1/trello';
+  const apiRoot = 'https://tranquil-ocean-70433.herokuapp.com/v1/';
+  const trelloApiRoot = 'https://tranquil-ocean-70433.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
